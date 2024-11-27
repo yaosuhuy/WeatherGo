f1e5698b220f1d343a64ebfcf3754c7398a76801
yaosuhuy <levanquanghuy8@gmail.com>
2024-11-27 16:08:20 +0700

    rss reader completed

--- app/src/main/java/com/example/weathergo/Fragments/NewsFragment.java
+++ app/src/main/java/com/example/weathergo/Fragments/NewsFragment.java
@@ -1,5 +1,6 @@
 package com.example.weathergo.Fragments;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,60 +8,85 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.example.weathergo.Parser.XMLParser;
 import com.example.weathergo.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link NewsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
 public class NewsFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public NewsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NewsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static NewsFragment newInstance(String param1, String param2) {
-        NewsFragment fragment = new NewsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    ListView listView;
+    ArrayList<String> arrayList = new ArrayList<String>();
+    ArrayList<String> arrayLink = new ArrayList<String>();
+    ArrayAdapter arrayAdapter;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        // Inflate the layout for this fragment
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news, container, false);
+        View view = inflater.inflate(R.layout.fragment_news, container, false);
+        listView = view.findViewById(R.id.newsListView);
+        AsyncTask<String, Void, String> content = new RssFeed().execute("https://thoitiethomnay.vn/rss/test.xml");
+        arrayAdapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(arrayAdapter);
+        return view;
+    }
+
+    public class RssFeed  extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            StringBuilder content = new StringBuilder();
+            try {
+                URL url = new URL(strings[0]);
+                InputStreamReader reader  = new InputStreamReader(url.openConnection().getInputStream());
+                BufferedReader bufferedReader = new BufferedReader(reader);
+                String line = "";
+                while ((line = bufferedReader.readLine())!= null){
+                    content.append(line);
+                }
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            return content.toString();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try {
+                XMLParser xmlParser = new XMLParser();
+                Document document = xmlParser.getDocument(s);
+                NodeList nodeList = document.getElementsByTagName("item");
+                for (int i = 0; i < nodeList.getLength(); i++) {
+                    Element element = (Element) nodeList.item(i);
+                    String title = xmlParser.getValue(element, "title");
+                    if (title != null) {
+                        arrayList.add(title);
+                        arrayLink.add(xmlParser.getValue(element, "link"));
+                    }
+                }
+                arrayAdapter.notifyDataSetChanged();
+            } catch (Exception e) {
+                e.printStackTrace(); // In lỗi để dễ debug
+            }
+        }
     }
 }
